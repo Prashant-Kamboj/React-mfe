301f43db37369cd0b2adac343ee675479d62e91a
Prashant Kamboj <prashantkamboj@Prashants-MacBook-Pro.local>
2022-01-03 17:40:50 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import MarketingComponent from './components/MarketingComponent';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 export default function App() {
     console.log('hello from container')
     return (
+        <BrowserRouter>
         <div>
             Hello from container for development !!!!!
+            <Header />
             <MarketingComponent />
         </div>
+        </BrowserRouter>
+        
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+import { StylesProvider} from '@material-ui/core/styles'
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
 const App = () => {
     return (
         <div>
-            Hi there from marketing
-        </div>
+        <StylesProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/pricing" component={Pricing}/>
+                    <Route path="/" component={Landing} />
+                </Switch>
+            </BrowserRouter>
+        </StylesProvider>
+    </div>
     )
 }
 
