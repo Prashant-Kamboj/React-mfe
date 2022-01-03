249086d3ff0f6f9de7b61c2f40ee76eedccf7dec
Prashant Kamboj <prashantkamboj@Prashants-MacBook-Pro.local>
2022-01-03 18:24:24 +0530

    changes for class name collision fix with style provider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import MarketingComponent from './components/MarketingComponent';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
+import React from "react";
+import MarketingComponent from "./components/MarketingComponent";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default function App() {
-    console.log('hello from container')
-    return (
-        <BrowserRouter>
+  console.log("hello from container");
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
-            Hello from container for development !!!!!
-            <Header />
-            <MarketingComponent />
+          <Header />
+          <MarketingComponent />
         </div>
-        </BrowserRouter>
-        
-    )
+      </StylesProvider>
+    </BrowserRouter>
+  );
 }
